07d647cd72af4be221325c89d8440b1150d1c840
ushamfzr <ushamfzr@gmail.com>
2026-07-28 16:01:25 +0530

    Update build.py

--- build.py
+++ build.py
@@ -6,13 +6,15 @@ import imageio_ffmpeg
 ffmpeg_exe = imageio_ffmpeg.get_ffmpeg_exe()
 ffmpeg_dir = os.path.dirname(ffmpeg_exe)
 
+sep = os.pathsep  # Automatically uses ';' on Windows and ':' on Mac/Linux
+
 PyInstaller.__main__.run([
     'app.py',
     '--name=SaveHub by UG',
     '--windowed',
-    '--add-data=templates;templates',
-    '--add-data=static;static',
-    f'--add-binary={ffmpeg_exe};.',
+    f'--add-data=templates{sep}templates',
+    f'--add-data=static{sep}static',
+    f'--add-binary={ffmpeg_exe}{sep}.',
     '--hidden-import=yt_dlp',
     '--hidden-import=imageio_ffmpeg',
     '--hidden-import=webview',
